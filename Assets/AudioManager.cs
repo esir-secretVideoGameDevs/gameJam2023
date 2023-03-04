25f4549f453e33deee4911b6a122cdacf82240ae
Léo Filoche <leofiloche.wk@gmail.com>
2023-03-04 12:56:19 +0100

    Ajout musique au début du jeu

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip[] playlist;
+    public AudioSource audiosource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audiosource.clip = playlist[0];
+        audiosource.Play();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
