f6c12dc9a444bab35047982f2ab99bad7ecc6db9
Kali-ki <kilian.cornec@protonmail.com>
2023-03-04 17:08:15 +0100

    Add 3 levels system

--- Assets/LoadScene2.cs
+++ Assets/LoadScene2.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadSpecificScene : MonoBehaviour
+public class LoadScene2 : MonoBehaviour
 {
     
     private void OnTriggerEnter2D(Collider2D collision){
 
         if(collision.CompareTag("Player")){
-            SceneManager.LoadScene("Level_02");
+            SceneManager.LoadScene("Niveau2");
         }
 
     }
