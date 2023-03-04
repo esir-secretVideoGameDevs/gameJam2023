f94d7e1ac830450afc91ff85f3e305fa878116b1
lruellou <louis.ruellou@etudiant.univ-rennes1.fr>
2023-03-04 22:27:47 +0100

    Correcting routing script

--- Assets/LoadSpecificScene.cs
+++ Assets/LoadSpecificScene.cs
@@ -3,14 +3,41 @@ using UnityEngine.SceneManagement;
 
 public class LoadSpecificScene : MonoBehaviour
 {
-    public string sceneName;
+    public string sceneToLoad;// Cursor.visible = false;
+    public int numOfNextLevel;
+    public bool isCurrentLevelBroken;
+    private static bool[] hasBroken = {false, false, false}; // correspondant au niveau 1, 2 et 3
 
     private void OnTriggerEnter2D(Collider2D collision){
-
         if(collision.CompareTag("Player")){
-            SceneManager.LoadScene(sceneName);
-        }
+            Cursor.visible = false;
 
+            // Check if the level is broken
+            if (isCurrentLevelBroken) {
+                hasBroken[numOfNextLevel-1] = true;
+            }
+
+            // load the correct level
+            if (numOfNextLevel==4) {
+                if (everythingIsBroken()) {
+                    Cursor.visible = true;
+                    SceneManager.LoadScene(sceneToLoad);
+                } else {
+                    string newSceneName = "Niveau1";
+                    SceneManager.LoadScene(newSceneName);
+                }
+            }
+            else if (!hasBroken[numOfNextLevel-1]) {
+                SceneManager.LoadScene(sceneToLoad);
+            } else {
+                string newSceneName = sceneToLoad + "_cassé";
+                SceneManager.LoadScene(newSceneName);
+            }
+        }
+    }
+
+    private bool everythingIsBroken() {
+        return (hasBroken[0] && hasBroken[1] && hasBroken[2]);
     }
 
 }
