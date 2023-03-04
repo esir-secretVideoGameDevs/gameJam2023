c60d2ceabe6c8a5018398a9ae6a5748bbaeb66b2
Kali-ki <kilian.cornec@protonmail.com>
2023-03-04 16:47:20 +0100

    Add Scene changment

---
+++ Assets/LoadSpecificScene.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+    
+    private void OnTriggerEnter2D(Collider2D collision){
+
+        if(collision.CompareTag("Player")){
+            SceneManager.LoadScene("Level_02");
+        }
+
+    }
+
+}
