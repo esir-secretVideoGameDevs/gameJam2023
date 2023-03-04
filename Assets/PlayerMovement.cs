ee9bf822fababfbcf676c1186789d990249a1797
Léo Filoche <leofiloche.wk@gmail.com>
2023-03-04 15:40:00 +0100

    Mise en place des sons

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -17,6 +17,9 @@ public class PlayerMovement : MonoBehaviour
     private bool isJumping;
     public bool isGrounded;
 
+    public AudioSource audioSource;
+    public AudioClip audioClip;
+
     void FixedUpdate()
     {
 
@@ -34,6 +37,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 targetVelocity = new Vector2(horizontalMove, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
 
+        if(!audioSource.isPlaying && horizontalMove!=0)audioSource.PlayOneShot(audioClip);
         if(isJumping){
             rb.AddForce(new Vector2(0f, jumpForce));
             isJumping = false;
