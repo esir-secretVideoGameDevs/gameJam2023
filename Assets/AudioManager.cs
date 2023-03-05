224fdd3c59f40c15e6730e723834a99ff4630739
Léo Filoche <leofiloche.wk@gmail.com>
2023-03-05 02:52:21 +0100

    recuperation info scene actuelle

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
@@ -10,14 +11,47 @@ public class AudioManager : MonoBehaviour
     void Start()
     {
         audiosource.clip = playlist[0];
-        audiosource.Play();   
+        audiosource.Play();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!audiosource.isPlaying){
+        if (!audiosource.isPlaying)
+        {
             audiosource.clip = playlist[0];
         }
+    }
+
+    private Scene sceneCourante;
+    void Awake()
+    {
+        GameObject[] musObjs = GameObject.FindGameObjectsWithTag("GameMusic");
+        if (musObjs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log(scene);
+        // bool  scasee =  cassee(scene);
+        // if (cassee(sceneCourante) !=scasee)
+        // {
+        //     // Il faut changer 
+        //     audiosource.Stop();
+        //     int indiceMusique = scasee?1:0;
+        //     audiosource.clip = playlist[indiceMusique];
+        // }
+        // sceneCourante =scene;
+    }
+
+    private bool cassee(Scene s)
+    {
+        return s.name.EndsWith("_cassé");
     }
 }
